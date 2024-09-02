3142962b9b5b6ab827c0872bca262f5d6a356fb7
EmaSantoro <emsantoro@frba.utn.edu.ar>
2024-09-02 17:11:51 -0300

    inicio

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/sisoputnfrba/tp-golang/cpu/globals"
+	"github.com/sisoputnfrba/tp-golang/cpu/utils"
+)
+
+func main() {
+	utils.ConfigurarLogger()
+	log.Println("Hola soy un log")
+
+	globals.ClientConfig = utils.IniciarConfiguracion("client/config.json")
+
+	if globals.ClientConfig == nil {
+		log.Fatalf("No se pudo cargar la configuración")
+	}
 }
