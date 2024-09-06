bfed9ddd376f0e3ee5eae32b852e92d40cb4b3f1
NicoVMGrafica <schkurkonicolas@gmail.com>
2024-09-06 02:59:00 +0000

    Agregando paquete a CPU

--- global/global.go
+++
@@ -1,8 +0,0 @@
-package global
-
-type Paquete struct {
-	ID      string `json:"ID"` //de momento es un string que indica desde donde sale el mensaje.
-	Mensaje string `json:"mensaje"`
-	Size    int16  `json:"size"`
-	Array   []rune `json:"array"`
-}
