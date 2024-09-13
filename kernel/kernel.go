83f63a8f6a033e48efc8f85088f14c86380d7999
NicoSchkurkoVM <schkurkonicolas@gmail.com>
2024-09-13 03:56:14 +0000

    borrador

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -11,7 +11,6 @@ import (
 
 func main() {
 	utils.ConfigurarLogger()
-
 	globals.ClientConfig = utils.IniciarConfiguracion("configsKERNEL/config.json")
 
 	if globals.ClientConfig == nil {
@@ -20,10 +19,17 @@ func main() {
 
 	puerto := globals.ClientConfig.Puerto
 
-	//utils.EnviarPaqueteACPU()
+	/*
+		Al iniciar el módulo Kernel, se creará un proceso inicial para que éste lo planifique.
+		Para poder inicializarlo se requerirá que el Kernel recibirá dos parámetros:
+		el nombre del archivo de pseudocódigo que deberá ejecutar y el tamaño del proceso para ser inicializado en Memoria,
+		el TID 0 creado por este proceso tendrá la prioridad máxima 0 (cero).
+	*/
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/mensaje", utils.RecibirMensaje) // buscar que es mux xd
-	http.ListenAndServe(":"+strconv.Itoa(puerto), mux)
+	// Funciones
+	http.HandleFunc("PUT /proceso", utils.iniciarProceso) // buscar que es mux xd
+
+	//Escuchar (bloqueante)
+	http.ListenAndServe(":"+strconv.Itoa(puerto), nil)
 
 }
